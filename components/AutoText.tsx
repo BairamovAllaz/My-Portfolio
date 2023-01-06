14dc534d341792368c6313eb929a9c76c390a9af
bairamov <ellezbayramov2004@gmail.com>
2023-01-06 21:41:14 +0300

    Some changes

---
+++ components/AutoText.tsx
@@ -0,0 +1,29 @@
+function AutoText()
+{ 
+    return(
+        <div className = "flex flex-row text-slate-200 text-[40px] mt-5 justify-center">
+                    <p className = "text-center">Hi, I am</p>
+                    <TypeAnimation
+                        className = "pl-4 text-[#72E2AE]"
+                        sequence={[
+                            'Allaz Bairamov',
+                            1000,
+                            'Software Enginner',
+                            2000,
+                            'Javascript Developer',
+                            2000,
+                            '.NET Developer',
+                            2000,
+                            'Backend Developer',
+                            2000,
+                            'Student'
+                        ]}
+                        wrapper="p"
+                        cursor={true}
+                        repeat={Infinity}
+                    />
+            </div>
+    )
+}
+
+export default AutoText;
--- components/ProfileBox.tsx
+++ components/ProfileBox.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { TypeAnimation } from 'react-type-animation';
 import Icon from './Icon';
 import Links from './Links'
+import AutoText from './AutoText';
 
 function ProfileBox() {
   return (
@@ -10,29 +11,8 @@ function ProfileBox() {
         <div>
             <Icon size={150}/>
         </div>
-        <div className = "">
-            <div className = "flex flex-row text-slate-200 text-[40px] mt-5 justify-center">
-                    <p className = "text-center">Hi, I am</p>
-                    <TypeAnimation
-                        className = "pl-4 text-[#72E2AE]"
-                        sequence={[
-                            'Allaz Bairamov',
-                            1000,
-                            'Software Enginner',
-                            2000,
-                            'Javascript Developer',
-                            2000,
-                            '.NET Developer',
-                            2000,
-                            'Backend Developer',
-                            2000,
-                            'Student'
-                        ]}
-                        wrapper="p"
-                        cursor={true}
-                        repeat={Infinity}
-                    />
-            </div>
+        <div>
+            <AutoText/>
             <p className = "text-slate-100 text-center mt-4 text-gray-400 flex justify-center ">
                 I am a frontend web developer. I can provide clean code and pixel perfect design. 
                 I also make website more & more interactive with web animations.
